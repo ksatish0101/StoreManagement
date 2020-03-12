713e1bae5eb5c1eee23f94faa3d4e1e2d30ca560
kris <ksatish0101@gmail.com>
2020-03-12 07:30:41 -0400

    initial commit

---
+++ CRUDApp/target/classes/data.sql
@@ -0,0 +1,8 @@
+insert into store(id, type,name,address,city,state,zip)
+values(10001, 'mall', 'America Mall','west Street','Chicago','IL','60006');
+
+insert into store(id, type,name,address,city,state,zip)
+values(10021, 'mall', 'East Mall','Cox Rd','Long Island','NY','60006');
+
+insert into store(id, type,name,address,city,state,zip)
+values(10031, 'mall', 'Broadway','Madison Street','Chicago','IL','60006');
